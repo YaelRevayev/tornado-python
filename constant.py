45480a9c0d3a49f6c2b313d860201ea97619e5b4
YaelRevayev <gexploreapp@gmail.com>
2024-02-26 13:12:06 +0200

    new function created - create dir for storing json files

--- constant.py
+++ constant.py
@@ -1,2 +1,3 @@
 PRIMARY_KEY = 'MDACODE'
 CSV_SRC_PATH ='src\MadaReports - MadaReports.csv'
+DIR_NAME='mada_reports'
--- csvtojson.py
+++ csvtojson.py
@@ -1,32 +1,41 @@
 import csv
 import json
 import constant
+import os
+
+def create_dir():
+    parent_dir = "D:/Pycharm projects/"
+    path = os.path.join(parent_dir, constant.DIR_NAME) 
+    os.mkdir(path) 
 
 def make_json(csvFilePath):
-     
     data = {}
-    # Open a csv reader called DictReader
+
     with open(csvFilePath, encoding='utf-8') as csvf:
         csvReader = csv.DictReader(csvf)
-        count_record_groups = 0
+        count_groups = 0
         count_rows = 0
         json_file_name=''
 
         for rows in csvReader:
-             
-            # Assuming a column named 'No' to
-            # be the primary key
             key = rows[constant.PRIMARY_KEY]
             data[key] = rows
             count_rows+=1
+
             if count_rows == 50000:
+                count_record_groups+=1
+                json_file_name = create_serial_json_file(count_groups)
                 records_to_json(data,json_file_name)
+                data = {}
+                count_rows = 0
                 
 
-def records_to_json(d):
-    pass
+def records_to_json(data, jsonFilePath):
+      with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
+        jsonf.write(json.dumps(data, indent=4))
 
-def generate_serial_json_file():
-    pass
-# Call the make_json function
+def create_serial_json_file(serial):
+    return open("./{}/mada{}".format(constant.DIR_NAME,serial), "x")
+
+create_dir()
 make_json(constant.CSV_SRC_PATH)
