9f6bd590582320441908891649326637e0b51a1b
YaelRevayev <gexploreapp@gmail.com>
2024-02-27 11:33:58 +0200

    added more empty blocks for tests

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ class TestFileConverting(unittest.TestCase):
     global dir_name
     dir_name = "Yael"
 
-    def test_creating_new_dir(self):
+    def test_creating_new_dir_valid_name(self):
         csvtojson.create_dir(dir_name)
         self.assertIn(
             dir_name,
@@ -26,13 +26,31 @@ class TestFileConverting(unittest.TestCase):
         with self.assertWarns(UserWarning):
             csvtojson.create_dir("")
 
-    def test_creation_of_json_file(self):
+    def test_creation_of_json_file_valid_args(self):
         csvtojson.write_dict_to_json(data, 0, dir_name)
         file_name = "{0}{1}".format(constant.JSON_FILE_PREFIX, 0)
         self.assertTrue(
             os.path.exists("./{0}/{1}".format(dir_name, file_name)),
             msg="File has not been created in current directory",
         )
+
+    def test_creation_of_json_file_invalid_dir_name(self):
+        pass
+
+    def test_creation_of_json_file_empty_dir(self):
+        pass
+
+    def test_creation_of_josn_file_negative_group_num(self):
+        pass
+
+    def test_csv_to_json_less_than_one_group(self):
+        pass
+
+    def test_csv_to_json_only_one_group(self):
+        pass
+
+    def test_csv_to_json_more_than_one_group(self):
+        pass
 
 
 if __name__ == "__main__":
