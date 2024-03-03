b5648945b8a8b0aab57944ba7dedde367f771e7a
YaelRevayev <gexploreapp@gmail.com>
2024-03-03 16:09:18 +0200

    changed path syntax

--- constant.py
+++ constant.py
@@ -1,5 +1,5 @@
 PRIMARY_KEY = "MDACODE"
-CSV_SRC_PATH = "src\MadaReports - MadaReports.csv"
+CSV_SRC_PATH = "./src/MadaReports - MadaReports.csv"
 DIR_NAME = "mada_reports"
 JSON_FILE_PREFIX = "mada"
 MAX_RECORDS_PER_FILE = 50000
